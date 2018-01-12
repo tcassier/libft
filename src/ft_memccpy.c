e9f42b78e181d2e0af2ed9069b3e9960b066fb97
Theo CASSIER <tcassier@e2r5p13.42.fr>
2018-01-12 22:25:30 +0100

    libft

--- src/ft_memccpy.c
+++ src/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: tcassier <tcassier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 19:30:17 by tcassier          #+#    #+#             */
-/*   Updated: 2018/01/12 19:31:28 by tcassier         ###   ########.fr       */
+/*   Updated: 2018/01/12 22:25:20 by tcassier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	{
 		((char*)dst)[index] = ((char*)src)[index];
 		if (((char*)src)[index] == (char)c)
-			return (&dst[index + 1]);
+			return (&(dst[index + 1]));
 		index++;
 	}
 	return (NULL);
